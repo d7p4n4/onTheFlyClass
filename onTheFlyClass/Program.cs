7cd76d9d3ad2a3c9d9a58c52b14b431c01bb028a
d7p4n4 <bercikezs199999999@gmail.com>
2019-10-15 11:24:32 +0100

    Add project files.

---
+++ onTheFlyClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace onTheFlyClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var csc = new CSharpCodeProvider();
+            var cc = csc.CreateCompiler();
+            CompilerParameters cp = new CompilerParameters();
+
+            cp.ReferencedAssemblies.Add("mscorlib.dll");
+            cp.ReferencedAssemblies.Add("System.dll");
+            StringBuilder sb = new StringBuilder();
+            // The string can contain any valid c# code
+            sb.Append("namespace Foo{");
+            sb.Append("using System;");
+            sb.Append("public static class MyClass{");
+            sb.Append("}}");
+            // "results" will usually contain very detailed error messages
+            var results = csc.CompileAssemblyFromSource(cp, sb.ToString());
+            Console.WriteLine(results.ToString());
+        }
+    }
+}
